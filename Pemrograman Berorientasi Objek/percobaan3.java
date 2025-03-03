92120ebb0b4de59f26d32536a9f78fbd26266e33
Fabyan Yafi Syahbaz <fabyan.yafi@gmail.com>
2025-03-03 21:38:49 +0700

    tugas percobaan 3 (p2)

---
+++ Pemrograman Berorientasi Objek/percobaan3.java
@@ -0,0 +1,33 @@
+class acaraTV {
+    private String StasiunTV;
+    private String Acara;
+
+    public String getStasiunTV() {
+        return StasiunTV;
+    }
+
+    public void setStasiunTV(String StasiunTV) {
+        this.StasiunTV = StasiunTV;
+    }
+
+    public String getAcara() {
+        return Acara;
+    }
+
+    public void setAcara(String Acara) {
+        this.Acara = Acara;
+    }
+
+    public void cetak(){
+        System.out.println("Acara " + Acara + " StasiunTV " + StasiunTV);
+    }
+}
+
+public class percobaan3 {
+    public static void main(String[] args) {
+        acaraTV a = new acaraTV();
+        a.setStasiunTV("Indosiar");
+        a.setAcara("Sinetron");
+        a.cetak();
+    }
+}
