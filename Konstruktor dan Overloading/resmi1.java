c8ccaa502920709ef3e822cccffa665fb1122800
Fabyan Yafi Syahbaz <fabyan.yafi@gmail.com>
2025-03-10 07:10:45 +0700

    tugas resmi 1 (p3)

---
+++ Konstruktor dan Overloading/resmi1.java
@@ -0,0 +1,25 @@
+class Mahasiswa {
+    private int nrp;
+    private String nama;
+
+    public Mahasiswa(int i, String n){
+        nrp = i;
+        nama = n;
+    }
+
+    public int getNrp(){
+        return nrp;
+    }
+
+    public String getNama(){
+        return nama;
+    }
+}
+
+public class resmi1 {
+    public static void main(String[] args) {
+        Mahasiswa mhs = new Mahasiswa(12345, "Jono");
+        System.out.println("NRP : " + mhs.getNrp());
+        System.out.println("Nama : " + mhs.getNama());
+    }
+}
